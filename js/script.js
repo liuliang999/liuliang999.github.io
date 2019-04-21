333d7206f0a21e7b298e3e0f41c9857ccf64e806
liuliang999 <1046588149@qq.com>
2019-04-21 23:06:09 +0800

    第五次提交

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
 // JavaScript Document
+/****************
+HEADDING
+****************/
+
+function showsearch(){ 
+	document.getElementById("search-suggest").style.display="block";
+}
+
+function showsearch2(){
+	document.getElementById("search-suggest2").style.display="block";
+}
+
+function showsearch3(){
+	document.getElementById("search-suggest3").style.display="block";
+}
+
+function showsearch4(){
+	document.getElementById("search-suggest4").style.display="block";
+}
+
+function showsearch5(){
+	document.getElementById("search-suggest5").style.display="block";
+}
+
+
+/****************
+首页-查看更多
+****************/
+
+function showdetail(){ 
+	document.getElementById("detail-index-i").style.display="block";
+	document.getElementById("content-index").style.display="none";
+}
+
+function hidedetail(){ 
+	document.getElementById("detail-index-i").style.display="none";
+	document.getElementById("content-index").style.display="block";
+}
+
+function showdetail(){ 
+	document.getElementById("detail-index-i").style.display="block";
+	document.getElementById("content-article").style.display="none";
+}
+
+function hidedetail(){ 
+	document.getElementById("detail-index-i").style.display="none";
+	document.getElementById("content-article").style.display="block";
+}
